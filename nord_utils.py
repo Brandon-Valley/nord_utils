d7fa6acdf6e24b44068ae793034e3278a992eb76
Brandon Valley <bavvh8@mst.edu>
2019-08-21 14:43:42 -0500

    connect by index works

--- nord_utils.py
+++ nord_utils.py
@@ -9,8 +9,14 @@ NORD_EXE_PATH = "C:\\Program Files (x86)\\NordVPN\\nordvpn" #>nordvpn -c -i 103.
 
 
 
+
+
 def ip_by_index(index):
     lines = txt_logger.readTxt(ips_txt_path)
+    
+    if(index < 0 or index > len(lines) - 1):
+        raise Exception('ERROR:  Index: ' + index + ' outside range of IPs: 0 - ' + len(lines) - 1)
+    
     return lines[index]
 
 
@@ -22,16 +28,23 @@ def disconnect():
 def connect_by_ip(ip):
     subprocess.call(NORD_EXE_PATH + ' -c -i ' + ip)
     
+    
+    
+def connect_by_index(index):
+    ip = ip_by_index(index)
+    disconnect()
+    connect_by_ip(ip)
+    
 
 
 
 
 
 
-
+connect_by_index(5)
 
 # disconnect()
-connect_by_ip('103.208.220.117')
+# connect_by_ip('103.208.220.117')
 # print(ip_by_index(2))
 # print(sys.argv)
 # index = int(sys.argv[1])
